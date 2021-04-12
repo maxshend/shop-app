78a7e9a93c7c4df92065e90c607a3b50a85491ae
maxshend <shendrik.mv@gmail.com>
2021-04-13 02:35:55 +0300

    Add react-rails.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,7 @@ import Turbolinks from "turbolinks"
 
 Rails.start()
 Turbolinks.start()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
