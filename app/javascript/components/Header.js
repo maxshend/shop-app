b60b03b3a166312f0896b5b12a7d2ed365bc9320
maxshend <shendrik.mv@gmail.com>
2021-08-29 22:37:26 +0300

    Load list of languages in the Headers select from API.

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -6,6 +6,10 @@ import { LANGUAGES } from '../packs/i18n';
 function Header() {
   const isInitialMount = useRef(true);
   const [_, i18n] = useTranslation();
+  const [languages, setLanguages] = useState([]);
+  useEffect(() => {
+    fetchLanguages(setLanguages, setError);
+  }, []);
   const [language, setLanguage] = useState(i18n.language);
   useEffect(() => {
     if (isInitialMount.current) {
@@ -16,21 +20,43 @@ function Header() {
 
     return;
   }, [language]);
+  const [error, setError] = useState(null);
 
   const changeHandler = (event) => {
     event.preventDefault();
     setLanguage(event.target.value);
   };
-  const languageOptions = LANGUAGES.map((language) => {
-    return <option key={language} value={language}>{language}</option>
-  });
 
-  return (
-    <div className="flex justify-end">
+  var languageSelect = null;
+  if (!error) {
+    const languageOptions = languages.map((language) => {
+      return <option key={language} value={language}>{language}</option>
+    });
+
+    languageSelect = (
       <select value={language} onChange={changeHandler}>
         {languageOptions}
       </select>
+    );
+  }
+
+  return (
+    <div className="flex justify-end">
+      {languageSelect}
     </div>
+  );
+}
+
+function fetchLanguages(setLanguages, setError) {
+  fetch('/api/v1/locales')
+  .then(res => res.json())
+  .then(
+    (res) => {
+      setLanguages(res.locales);
+    },
+    (err) => {
+      setError(err);
+    }
   );
 }
 
--- app/javascript/packs/i18n.js
+++ app/javascript/packs/i18n.js
@@ -3,11 +3,8 @@ import { initReactI18next } from "react-i18next";
 
 import Backend from 'i18next-http-backend';
 
-export const LANGUAGES = ['en', 'ua', 'ru'];
-
 i18n.use(Backend).use(initReactI18next).init({
-  fallbackLng: LANGUAGES[0],
-  supportedLngs: LANGUAGES,
+  fallbackLng: 'en',
   debug: false,
   interpolation: {
     escapeValue: false
